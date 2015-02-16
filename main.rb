64daa5b484dadeec97b9228968a0acdbdc50e386
Jason Hobbs <grimmstede@feemon.com>
2015-02-16 14:59:55 -0600

    added cards and deck

--- main.rb
+++ main.rb
@@ -8,8 +8,7 @@ set :sessions, true
 
 get '/' do
   if session['username']
-    @username = session['username']
-    erb :hometemplate
+    redirect '/game'
   else
     erb :form
   end
@@ -19,4 +18,21 @@ end
 post '/getname' do
   session['username'] = params['username']
   redirect '/'
+end
+
+get '/game' do
+  if session['username']
+    suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    session[:deck] = suits.product(values).shuffle!
+    session[:dealer_cards] = []
+    session[:player_cards] = []
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+    erb :game
+  else
+    redirect '/'
+  end
 end
